fa46e5e5c648343af22146ed8f01a6a647f14ef4
worldofmarcus <me@marcuseriksson.com>
2022-09-23 13:48:37 +0000

    Add urls to global and local  urls.py and create base.html

--- reviewalliance/urls.py
+++ reviewalliance/urls.py
@@ -19,4 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-]
+    path('', include('reviews.urls'), name='reviews_urls'),
