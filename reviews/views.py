1f41de5648114616b22ea26329488d3d232756d3
worldofmarcus <me@marcuseriksson.com>
2022-09-23 13:38:55 +0000

    Add HomeView in views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post
 
-# Create your views here.
+class HomeView(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1, category='Album').order_by('-date_created_on')
+    template_name = ('index.html')
+    paginate_by = 6
