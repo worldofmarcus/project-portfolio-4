c83dd031453b927e8959214df08780af88325057
worldofmarcus <me@marcuseriksson.com>
2022-09-23 13:41:50 +0000

    Add Detailview and About function to views.py

--- reviews/views.py
+++ reviews/views.py
@@ -13,3 +13,20 @@ class ConcertView(generic.ListView):
     queryset = Post.objects.filter(status=1, category='Concert').order_by('-date_created_on')
     template_name = ('concert_reviews.html')
     paginate_by = 6
+
+def about(request):
+    return render(request, 'about.html')
+
+class DetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('date_created_on')
+        return render(
+            request,
+            "review_details.html",
+            {
+                "post": post,
+                "comments": comments,
+            },
+        )
