f6449a35c89eaaf2da7501d265d1c4dcb90da2e4
worldofmarcus <me@marcuseriksson.com>
2022-09-24 10:40:02 +0000

    Add authorization functionality to site

--- reviewalliance/urls.py
+++ reviewalliance/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('reviews.urls'), name='reviews_urls'),
+    path('members/', include('allauth.urls')),
 ]
