3d1079b90c5363659dd0e06af9ee4fd2614ab655
worldofmarcus <me@marcuseriksson.com>
2022-09-29 07:37:50 +0000

    Add docstrings to models.py and tidy up code

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,5 @@
+"""Import relevant modules for the application"""
+
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -5,22 +7,49 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, 'Draft'), (1, 'Published'))
 RATING_CHOICES = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
 
+
 class Category(models.Model):
+    """
+    This class creates the category model.
+    """
+
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
+        """
+        This meta class orders the model by name.
+        """
+
         ordering = ['name']
 
     def __str__(self):
+        """
+        This function returns the name of the category
+        to add user readability.
+        """
+
         return str(self.name)
 
+
 class Genre(models.Model):
+    """
+    This class creates the genre model.
+    """
+
     name = models.CharField(max_length=255, unique=True)
 
     class Meta:
+        """
+        This meta class orders the model by name.
+        """
         ordering = ['name']
 
     def __str__(self):
+        """
+        This function returns the name of the genre
+        to add user readability.
+        """
+
         return str(self.name)
 
 
@@ -36,6 +65,10 @@ for item in GENRE_CHOICES:
 
 
 class Post(models.Model):
+    """
+    This class creates the post model.
+    """
+
     title = models.CharField(max_length=255, unique=True)
     artist = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -44,30 +77,51 @@ class Post(models.Model):
     featured_image = CloudinaryField('image', default='default_image')
     fragment = models.TextField(blank=True)
     body = models.TextField()
-    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
-    category = models.CharField(max_length=255, default='Uncategorized', choices=choice_list)
+    likes = models.ManyToManyField(User, related_name='review_likes',
+                                   blank=True)
+    category = models.CharField(max_length=255, default='Uncategorized',
+                                choices=choice_list)
     status = models.IntegerField(choices=STATUS, default=0)
     date_created_on = models.DateTimeField(auto_now_add=True)
     date_updated_on = models.DateTimeField(auto_now=True)
     release_live_date = models.DateTimeField()
     record_label = models.CharField(max_length=255, blank=True)
     venue = models.CharField(max_length=255, blank=True)
-    genre = models.CharField(max_length=255, default='Uncategorized', choices=genre_list)
+    genre = models.CharField(max_length=255, default='Uncategorized',
+                             choices=genre_list)
     rating = models.IntegerField(choices=RATING_CHOICES, default=3)
     approved = models.BooleanField(default=False)
 
     class Meta:
+        """
+        This meta class orders the model by date.
+        """
+
         ordering = ['-date_created_on']
 
     def __str__(self):
+        """
+        This function returns the title and the name of the author
+        to add user readability.
+        """
+
         return self.title + ' || ' + str(self.author)
 
     def number_of_likes(self):
+        """
+        This function returns the like count.
+        """
+
         return self.likes.count()
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    """
+    This class creates the comment model.
+    """
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
     name = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="review_comments")
@@ -77,7 +131,16 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
+        """
+        This meta class orders the model by date.
+        """
+
         ordering = ['date_created_on']
 
     def __str__(self):
+        """
+        This function returns the body and the name of the author
+        to add user readability.
+        """
+
         return self.body + ' || ' + str(self.name)
