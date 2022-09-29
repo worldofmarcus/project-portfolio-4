8453d4727e26a399de626820a58536bd57aab029
worldofmarcus <me@marcuseriksson.com>
2022-09-29 07:27:06 +0000

    Tidy up code in urls.py and add module string

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,17 +1,29 @@
+"""Import relevant modules for the application"""
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('concert-reviews/', views.ConcertView.as_view(), name='concert_reviews'),
-    path('album-reviews/', views.AlbumView.as_view(), name='album_reviews'),
-    path('member-reviews/', views.MemberReviewView.as_view(), name='member_reviews'),
-    path('create-review/', views.CreateReview.as_view(), name='create_review'),
+    path('concert-reviews/', views.ConcertView.as_view(),
+         name='concert_reviews'),
+    path('album-reviews/', views.AlbumView.as_view(),
+         name='album_reviews'),
+    path('member-reviews/', views.MemberReviewView.as_view(),
+         name='member_reviews'),
+    path('create-review/', views.CreateReview.as_view(),
+         name='create_review'),
     path('about/', views.about, name='about'),
-    path('<slug:slug>/', views.DetailView.as_view(), name='review_details'),
-    path('<slug:slug>/edit/', views.UpdateReview.as_view(), name='update_review'),
-    path('<int:pk>/delete/', views.DeleteComment.as_view(), name='delete_comment'),
-    path('<slug:slug>/delete/', views.DeleteReview.as_view(), name='delete_review'),
-    path('<int:pk>/update-comment/', views.UpdateComment.as_view(), name='update_comment'),
-    path('like/<slug:slug>', views.ReviewLike.as_view(), name='like_review'),
+    path('<slug:slug>/', views.DetailView.as_view(),
+         name='review_details'),
+    path('<slug:slug>/edit/', views.UpdateReview.as_view(),
+         name='update_review'),
+    path('<int:pk>/delete/', views.DeleteComment.as_view(),
+         name='delete_comment'),
+    path('<slug:slug>/delete/', views.DeleteReview.as_view(),
+         name='delete_review'),
+    path('<int:pk>/update-comment/', views.UpdateComment.as_view(),
+         name='update_comment'),
+    path('like/<slug:slug>', views.ReviewLike.as_view(),
+         name='like_review'),
 ]
