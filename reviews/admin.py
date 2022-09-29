c528c972b983a9102b556a634178edd0cefaf867
worldofmarcus <me@marcuseriksson.com>
2022-09-29 08:00:16 +0000

    Add docstrings to admin.py and tidy up code

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,40 +1,79 @@
+"""Import relevant modules for the application"""
+
 from django.contrib import admin
-from .models import Post, Category, Comment, Genre
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Category, Comment, Genre
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    This class adds fields from the post model to the admin
+    area and also add functionality like approve review and
+    publish in the action dropdown list.
+    """
+
     summernote_fields = ('body')
-    list_display = ('title', 'slug', 'category', 'status', 'date_created_on', 'author', 'approved', 'release_live_date',)
+    list_display = ('title', 'slug', 'category', 'status', 'date_created_on',
+                    'author', 'approved', 'release_live_date',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'date_created_on')
     actions = ['approve_review', 'publish']
 
-    def approve_review(self, request, queryset):
+    def approve_review(self, queryset):
+        """
+        This help method updates the approved field to True
+        """
+
         queryset.update(approved=True)
 
-    def publish(self, request, queryset):
+    def publish(self, queryset):
+        """
+        This help method updates the status field to 1
+        """
+
         queryset.update(status=1)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """
+    This class adds fields from the category model to the admin
+    area and also add list functionality.
+    """
+
     list_display = ('name', )
     list_filter = ('name', )
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'date_created_on', 'approved', 'author')
+    """
+    This class adds fields from the comment model to the admin
+    area and also add functionality like approve comments in the
+    action dropdown list.
+    """
+
+    list_display = ('name', 'body', 'post', 'date_created_on', 'approved',
+                    'author')
     list_filter = ('approved', 'date_created_on')
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
 
-    def approve_comments(self, request, queryset):
+    def approve_comments(self, queryset):
+        """
+        This help method updates the approved field to True
+        """
         queryset.update(approved=True)
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    """
+    This class adds fields from the category model to the admin
+    area and also add list functionality.
+    """
+
     list_display = ('name', )
     list_filter = ('name', )
