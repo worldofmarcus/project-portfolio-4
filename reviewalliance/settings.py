06ec832d23e0f8546b26aa65e293eaa8d7c80fe1
worldofmarcus <me@marcuseriksson.com>
2022-10-11 06:56:29 +0000

    Change debug to false to test deploy to Heroku

--- reviewalliance/settings.py
+++ reviewalliance/settings.py
@@ -30,7 +30,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
